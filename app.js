42eccd1cfd6648d26b2ed17e4937cfb98917af96
Jaya kumar <jayakv1995@gmail.com>
2023-11-03 21:10:37 +0530

    Authentication 3 using Hashing npm md5

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import bodyParser from "body-parser";
 import ejs from "ejs";
 import mongoose from "mongoose";
 import encrypt from "mongoose-encryption";
+import md5 from "md5";
 
 const app = express();
 const port = 3000;
@@ -20,8 +21,6 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
 
 
 const User = new mongoose.model("User", userSchema);
@@ -46,7 +45,7 @@ app.post('/register',(req,res)=>{
     console.log(req.body);
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save().then(
         (result) => {
@@ -63,7 +62,7 @@ app.post('/register',(req,res)=>{
 app.post("/login",(req,res)=>{
 
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     console.log(username,password);
 
     User.findOne({email: username}).then(
