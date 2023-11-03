413af9949a26a980d5b7d039ab7ffa74e506d42c
Jaya kumar <jayakv1995@gmail.com>
2023-11-03 20:37:38 +0530

    configured dotenv to avoid secrets

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+import 'dotenv/config'
 import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
@@ -20,8 +21,7 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Thisisourlittlesecret";
-userSchema.plugin(encrypt, { secret: secret , encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET , encryptedFields: ['password'] });
 
 
 const User = new mongoose.model("User", userSchema);
